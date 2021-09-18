2a189489b5e24f2ead68f0cdb559714b173aa083
Enrique Ramirez <nrqrmz@gmail.com>
2021-09-18 17:58:52 -0500

    Create subcategory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_225632) do
+ActiveRecord::Schema.define(version: 2021_09_18_225817) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2021_09_18_225632) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["reviews_id"], name: "index_products_on_reviews_id"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "subcategory_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,5 +72,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_225632) do
 
   add_foreign_key "products", "reviews", column: "reviews_id"
   add_foreign_key "products", "users"
+  add_foreign_key "subcategories", "categories"
   add_foreign_key "users", "requests"
 end
