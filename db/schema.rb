05cf823d1f42b3123e41a140fbb6fb5d9a98e2b3
Enrique Ramirez <nrqrmz@gmail.com>
2021-09-18 18:00:36 -0500

    Create bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_225817) do
+ActiveRecord::Schema.define(version: 2021_09_18_230003) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_bookmarks_on_product_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -70,6 +79,8 @@ ActiveRecord::Schema.define(version: 2021_09_18_225817) do
     t.index ["request_id"], name: "index_users_on_request_id"
   end
 
+  add_foreign_key "bookmarks", "products"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "products", "reviews", column: "reviews_id"
   add_foreign_key "products", "users"
   add_foreign_key "subcategories", "categories"
