7bfde3331849ffe23e173255eb6f56bf9f038077
Enrique Ramirez <nrqrmz@gmail.com>
2021-09-18 17:57:02 -0500

    Create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_225129) do
+ActiveRecord::Schema.define(version: 2021_09_18_225632) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.integer "user_id", null: false
